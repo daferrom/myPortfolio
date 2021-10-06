2e1c745fdc2add8ce94dd14840375e7cca713573
daferrom <daferrom@gmail.com>
2021-10-05 22:31:03 -0500

    trying to apply an slider t show projects

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Portafolio2
+
+This is the imrpoved version of my initial portfolio
--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+const btn_menu = document.querySelector('.btn_menu')
+
 addEventListener('DOMContentLoaded',()=>{
-    const btn_menu = document.querySelector('.btn_menu')
     if (btn_menu){
         btn_menu.addEventListener('click', ()=>{
-            const menu_items =document.querySelector('.menu_items')
+            const menu_items = document.querySelector('.menuItems')
             menu_items.classList.toggle('show')
         })
     }
 })
+
